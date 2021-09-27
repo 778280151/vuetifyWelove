553d10cc19e129a570815b8c841e11d83273961c
zhangchongjie <778280151@qq.com>
2021-09-27 15:36:54 +0800

    更新

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -42,11 +42,11 @@
           mini-variant
       >
 
-
         <v-avatar
             class="d-block text-center mx-auto mt-4"
             color="grey darken-1"
             size="36"
+            @click="alert('fff')"
         ></v-avatar>
 
         <v-divider class="mx-3 my-5"></v-divider>
@@ -123,7 +123,8 @@ export default {
   },
   data: () => ({
     drawer: null
-  })
+  }),
+  methods: {}
 }
 </script>
 
